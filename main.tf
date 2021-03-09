b02241885bcc4f2b6c71d9f952eaadc78e18bf43
Damon Green <damon.green@btinternet.com>
2021-03-09 10:01:09 +0000

    comment RG, remove image list

--- main.tf
+++ main.tf
@@ -7,14 +7,14 @@ locals {
 
     ]
 }
-/*
+
 resource "azurerm_resource_group" "rg" {
   name     = "${var.product}-${var.env}"
   location = var.location
 
   tags = var.common_tags
 }
-*/
+
 module "key-vault" {
   source              = "git@github.com:hmcts/cnp-module-key-vault?ref=master"
   product             = var.product
