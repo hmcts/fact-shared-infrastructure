51d358877c3ee7a06181940abf44c7f078c01542
Damon Green <damon.green@btinternet.com>
2021-03-10 12:34:28 +0000

    add initial AI

--- main.tf
+++ main.tf
@@ -40,6 +40,23 @@ resource "azurerm_application_insights" "appinsights2" {
   }
 }
 
+resource "azurerm_application_insights" "appinsights" {
+  name                = "${var.product}-appinsights-${var.env}"
+  location            = var.appinsights_location
+  resource_group_name = azurerm_resource_group.rg2.name
+  application_type    = "web"
+
+  tags = var.common_tags
+
+  lifecycle {
+    ignore_changes = [
+      # Ignore changes to appinsights as otherwise upgrading to the Azure provider 2.x
+      # destroys and re-creates this appinsights instance
+      application_type,
+    ]
+  }
+}
+
 /*
 module "key-vault" {
   source              = "git@github.com:hmcts/cnp-module-key-vault?ref=master"
