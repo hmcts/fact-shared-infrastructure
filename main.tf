bb7cd41bca3690ddaa7d4a05c79f9fd2f7552f39
“Thomas <32871802+Thomas-Geraghty@users.noreply.github.com>
2021-04-29 15:34:58 +0100

    Reverted accidentally pushing to master.

--- main.tf
+++ main.tf
@@ -23,7 +23,7 @@ module "key-vault" {
   resource_group_name = azurerm_resource_group.rg.name
 
   # dcd_platformengineering group object ID
-  product_group_name         = "DTS FaCT"
+  product_group_object_id    = "c36eaede-a0ae-4967-8fed-0a02960b1370"
   common_tags                = var.common_tags
   create_managed_identity    = true
 }
