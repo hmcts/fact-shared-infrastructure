4dc69e0b59ac46b773987642d951dc1da996dfb3
Josh <23316525+josh-uk@users.noreply.github.com>
2025-10-01 16:13:47 +0100

    Updated AI for alert limit cap (#90)

--- app_insights.tf
+++ app_insights.tf
@@ -2,10 +2,11 @@
 module "app_insights" {
   source = "git@github.com:hmcts/terraform-module-application-insights?ref=4.x"
 
-  env      = var.env
-  product  = var.product
-  name     = "${var.product}-ai"
-  location = var.appinsights_ai_location
+  env                 = var.env
+  product             = var.product
+  name                = "${var.product}-ai"
+  location            = var.appinsights_ai_location
+  alert_limit_reached = true
 
   resource_group_name = azurerm_resource_group.rg.name
   common_tags         = var.common_tags
