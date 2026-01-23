486549a0d5bc8ad88a0186db0e2cc4ca33fa8a2a
hurricanepilot <jonathan.coomber@justice.gov.uk>
2026-01-23 14:17:52 +0000

    Add terraform config for pulling in bootstrap secrets

---
+++ keyvault-bootstrap.tf
@@ -0,0 +1,44 @@
+locals {
+  # Common secrets for all envs
+  base_bootstrap_secrets = [
+    "app-reg-id",
+    "cath-api-url",
+    "os-key",
+    "slack-channel-id",
+    "slack-token"
+  ]
+
+  # functional tests run on the AAT environment and require additional secrets
+  aat_bootstrap_secrets = [
+    "testing-client-app-id",
+    "testing-client-secret",
+    "testing-tenant-id"
+  ]
+
+  bootstrap_secrets = var.env == "aat" ? concat(local.base_bootstrap_secrets, local.aat_bootstrap_secrets) : local.base_bootstrap_secrets
+  bootstrap_prefix  = "${var.product}-bstrap-${var.env}"
+  bootstrap_name_prefix = "bstrap";
+}
+
+data "azurerm_key_vault" "bootstrap_kv" {
+  name                = "${local.bootstrap_prefix}-kv"
+  resource_group_name = "${local.bootstrap_prefix}-rg"
+}
+
+data "azurerm_key_vault_secret" "bootstrap_secrets" {
+  for_each     = { for secret in local.bootstrap_secrets : secret => secret }
+  name         = each.value
+  key_vault_id = data.azurerm_key_vault.bootstrap_kv.id
+}
+
+resource "azurerm_key_vault_secret" "bootstrap_secrets" {
+  for_each        = data.azurerm_key_vault_secret.bootstrap_secrets
+  key_vault_id    = module.this.key_vault_id
+  name            = "${local.bootstrap_name_prefix}-${each.value.name}"
+  value           = each.value.value
+  tags            = merge(var.tags, {
+    "source" : "bootstrap ${data.azurerm_key_vault.bootstrap_kv.name} secrets"
+  })
+  content_type    = "Manual Secret"
+  expiration_date = timeadd(timestamp(), "17520h")
+}
