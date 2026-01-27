635b1553714af6267d5192a7a0c815fa0cbd73f9
Jon <hurricanepilot@gmail.com>
2026-01-27 13:38:01 +0000

    FACT-2642 add bootstrap secret support (#95)
    * Add terraform config for pulling in bootstrap secrets
    
    * Changed name and reduced bootstrap secret set
    
    * switched to common_tags as that's actually defined.
    
    * fixed formatting errors in tf file
    
    * Updating Terraform Formatting
    
    * corrected module reference for key_vault_id
    
    * added comment to telegraph config is for new FaCT
    
    * Add configuration for calculatable secrets (new FaCT)
    
    * Updating Terraform Formatting
    
    * change provider name to azurerm
    
    * switch to the correct data source reference
    
    * added "data." prefix to dereferences
    
    * added "azuread_application" specified to data deference
    
    * fix tags and update tenant-id secret for testing
    
    * added session secret configuration and secret
    
    ---------
    
    Co-authored-by: hurricanepilot <jonathan.coomber@justice.gov.uk>
    Co-authored-by: 58060 <62423932+hmcts-jenkins-d-to-i[bot]@users.noreply.github.com>

---
+++ keyvault-bootstrap-secrets.tf
@@ -0,0 +1,42 @@
+# These are the bootstrap secrets used by the new FaCT
+locals {
+  # Common secrets for all envs
+  base_bootstrap_secrets = [
+    "cath-api-url",
+    "os-key",
+    "slack-channel-id",
+    "slack-token"
+  ]
+
+  # functional tests run on the AAT environment and require additional secrets
+  aat_bootstrap_secrets = [
+    "func-test-client-secret"
+  ]
+
+  bootstrap_secrets     = var.env == "aat" ? concat(local.base_bootstrap_secrets, local.aat_bootstrap_secrets) : local.base_bootstrap_secrets
+  bootstrap_prefix      = "${var.product}-bstrap-${var.env}"
+  bootstrap_name_prefix = "bstrap"
+}
+
+data "azurerm_key_vault" "bootstrap_kv" {
+  name                = "${local.bootstrap_prefix}-kv"
+  resource_group_name = "${local.bootstrap_prefix}-rg"
+}
+
+data "azurerm_key_vault_secret" "bootstrap_secrets" {
+  for_each     = { for secret in local.bootstrap_secrets : secret => secret }
+  name         = each.value
+  key_vault_id = data.azurerm_key_vault.bootstrap_kv.id
+}
+
+resource "azurerm_key_vault_secret" "bootstrap_secrets" {
+  for_each     = data.azurerm_key_vault_secret.bootstrap_secrets
+  key_vault_id = module.key_vault.key_vault_id
+  name         = "${local.bootstrap_name_prefix}-${each.value.name}"
+  value        = each.value.value
+  tags = merge(var.common_tags, {
+    "source" : "bootstrap ${data.azurerm_key_vault.bootstrap_kv.name} secrets"
+  })
+  content_type    = "Manual Secret"
+  expiration_date = timeadd(timestamp(), "17520h")
+}
---
+++ keyvault-secrets.tf
@@ -0,0 +1,65 @@
+# These are the calculated secrets used by the new FaCT
+locals {
+  app_reg_suffix           = var.env == "prod" ? "prod" : "non-prod"
+  api_app_reg_name         = "fact-data-api-${local.app_reg_suffix}"
+  test_client_app_reg_name = "fact-admin-frontend-${local.app_reg_suffix}"
+}
+
+resource "random_password" "session_string" {
+  length      = 20
+  min_upper   = 2
+  min_lower   = 2
+  min_numeric = 2
+  min_special = 2
+  special     = true
+}
+
+data "azuread_application" "api_app_reg" {
+  display_name = local.api_app_reg_name
+}
+
+data "azuread_application" "test_client_app_reg" {
+  display_name = local.test_client_app_reg_name
+}
+
+resource "azurerm_key_vault_secret" "api_app_reg_id" {
+  name         = "api-app-reg-id"
+  value        = data.azuread_application.api_app_reg.client_id
+  key_vault_id = module.key_vault.key_vault_id
+  tags = merge(var.common_tags, {
+    "source" : "calculated from ${local.api_app_reg_name} app reg"
+  })
+  content_type    = "Calculated Secret"
+  expiration_date = timeadd(timestamp(), "17520h")
+}
+
+resource "azurerm_key_vault_secret" "test_client_app_reg_id" {
+  count        = var.env == "aat" ? 1 : 0
+  name         = "func-test-client-app-id"
+  value        = data.azuread_application.test_client_app_reg.client_id
+  key_vault_id = module.key_vault.key_vault_id
+  tags = merge(var.common_tags, {
+    "source" : "calculated from ${local.test_client_app_reg_name} app reg"
+  })
+  content_type    = "Calculated Secret"
+  expiration_date = timeadd(timestamp(), "17520h")
+}
+
+resource "azurerm_key_vault_secret" "test_client_tenant_id" {
+  count           = var.env == "aat" ? 1 : 0
+  name            = "func-test-tenant-id"
+  value           = var.tenant_id
+  key_vault_id    = module.key_vault.key_vault_id
+  tags            = var.common_tags
+  content_type    = "Calculated Secret"
+  expiration_date = timeadd(timestamp(), "17520h")
+}
+
+resource "azurerm_key_vault_secret" "session_secret" {
+  name            = "session-secret"
+  value           = random_password.session_string.result
+  key_vault_id    = module.key_vault.key_vault_id
+  tags            = var.common_tags
+  content_type    = "Calculated Secret"
+  expiration_date = timeadd(timestamp(), "17520h")
+}
