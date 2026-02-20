b8c6fbc41d092e6e0e89859a87f64c6048ae4297
Josh <23316525+josh-uk@users.noreply.github.com>
2026-02-20 14:25:18 +0000

    Added new secrets for testing (#98)

--- keyvault-secrets.tf
+++ keyvault-secrets.tf
@@ -3,6 +3,7 @@ locals {
   app_reg_suffix           = var.env == "prod" ? "prod" : "non-prod"
   api_app_reg_name         = "fact-data-api-${local.app_reg_suffix}"
   test_client_app_reg_name = "fact-admin-frontend-${local.app_reg_suffix}"
+  test_viewer_app_reg_name = "fact-frontend-${local.app_reg_suffix}"
 }
 
 resource "random_password" "session_string" {
@@ -20,6 +21,10 @@ data "azuread_application" "api_app_reg" {
 
 data "azuread_application" "test_client_app_reg" {
   display_name = local.test_client_app_reg_name
+}
+
+data "azuread_application" "test_viewer_app_reg" {
+  display_name = local.test_viewer_app_reg_name
 }
 
 resource "azurerm_key_vault_secret" "api_app_reg_id" {
@@ -40,6 +45,18 @@ resource "azurerm_key_vault_secret" "test_client_app_reg_id" {
   key_vault_id = module.key_vault.key_vault_id
   tags = merge(var.common_tags, {
     "source" : "calculated from ${local.test_client_app_reg_name} app reg"
+  })
+  content_type    = "Calculated Secret"
+  expiration_date = timeadd(timestamp(), "17520h")
+}
+
+resource "azurerm_key_vault_secret" "test_client_viewer_app_reg_id" {
+  count        = var.env == "aat" ? 1 : 0
+  name         = "func-test-viewer-client-app-id"
+  value        = data.azuread_application.test_viewer_app_reg.client_id
+  key_vault_id = module.key_vault.key_vault_id
+  tags = merge(var.common_tags, {
+    "source" : "calculated from ${local.test_viewer_app_reg_name} app reg"
   })
   content_type    = "Calculated Secret"
   expiration_date = timeadd(timestamp(), "17520h")
