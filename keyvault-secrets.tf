0f0c5a643d1f2ae5aad6795167beb020898be29d
Josh <23316525+josh-uk@users.noreply.github.com>
2026-03-04 17:24:37 +0000

    Added public frontend ID for use in all envs

--- keyvault-secrets.tf
+++ keyvault-secrets.tf
@@ -3,7 +3,7 @@ locals {
   app_reg_suffix           = var.env == "prod" ? "prod" : "non-prod"
   api_app_reg_name         = "fact-data-api-${local.app_reg_suffix}"
   test_client_app_reg_name = "fact-admin-frontend-${local.app_reg_suffix}"
-  test_viewer_app_reg_name = "fact-frontend-${local.app_reg_suffix}"
+  public_frontend_app_reg_name = "fact-frontend-${local.app_reg_suffix}"
 }
 
 resource "random_password" "session_string" {
@@ -23,13 +23,24 @@ data "azuread_application" "test_client_app_reg" {
   display_name = local.test_client_app_reg_name
 }
 
-data "azuread_application" "test_viewer_app_reg" {
-  display_name = local.test_viewer_app_reg_name
+data "azuread_application" "public_frontend_app_reg" {
+  display_name = local.public_frontend_app_reg_name
 }
 
 resource "azurerm_key_vault_secret" "api_app_reg_id" {
   name         = "api-app-reg-id"
   value        = data.azuread_application.api_app_reg.client_id
+  key_vault_id = module.key_vault.key_vault_id
+  tags = merge(var.common_tags, {
+    "source" : "calculated from ${local.api_app_reg_name} app reg"
+  })
+  content_type    = "Calculated Secret"
+  expiration_date = timeadd(timestamp(), "17520h")
+}
+
+resource "azurerm_key_vault_secret" "public_frontend_app_reg_id" {
+  name         = " public-frontend-app-reg-id"
+  value        = data.azuread_application.public_frontend_app_reg.client_id
   key_vault_id = module.key_vault.key_vault_id
   tags = merge(var.common_tags, {
     "source" : "calculated from ${local.api_app_reg_name} app reg"
@@ -53,7 +64,7 @@ resource "azurerm_key_vault_secret" "test_client_app_reg_id" {
 resource "azurerm_key_vault_secret" "test_client_viewer_app_reg_id" {
   count        = var.env == "aat" ? 1 : 0
   name         = "func-test-viewer-client-app-id"
-  value        = data.azuread_application.test_viewer_app_reg.client_id
+  value        = data.azuread_application.public_frontend_app_reg.client_id
   key_vault_id = module.key_vault.key_vault_id
   tags = merge(var.common_tags, {
     "source" : "calculated from ${local.test_viewer_app_reg_name} app reg"
