0a501d557f761db5cfa9b05c7088be59dd833f4b
debolina-seal <100166232+debolina-seal@users.noreply.github.com>
2026-07-22 08:11:36 +0100

    adding fact-cron-trigger-app-reg-id (#126)
    * adding fact-cron-trigger-app-reg-id
    
    * adding fact-cron-trigger-app-reg-id

--- keyvault-secrets.tf
+++ keyvault-secrets.tf
@@ -4,6 +4,7 @@ locals {
   api_app_reg_name             = "fact-data-api-${local.app_reg_suffix}"
   admin_frontend_app_reg_name  = "fact-admin-frontend-${local.app_reg_suffix}"
   public_frontend_app_reg_name = "fact-frontend-${local.app_reg_suffix}"
+  fact_cron_trigger_app_reg_name = "fact-cron-trigger-${local.app_reg_suffix}"
 }
 
 resource "random_password" "session_string" {
@@ -25,6 +26,10 @@ data "azuread_application" "admin_app_reg" {
 
 data "azuread_application" "public_frontend_app_reg" {
   display_name = local.public_frontend_app_reg_name
+}
+
+data "azuread_application" "fact_cron_trigger_app_reg" {
+  display_name = local.fact_cron_trigger_app_reg_name
 }
 
 resource "azurerm_key_vault_secret" "api_app_reg_id" {
@@ -55,6 +60,16 @@ resource "azurerm_key_vault_secret" "admin_frontend_app_reg_id" {
   key_vault_id = module.key_vault.key_vault_id
   tags = merge(var.common_tags, {
     "source" : "calculated from ${local.admin_frontend_app_reg_name} app reg"
+  })
+  content_type    = "Calculated Secret"
+  expiration_date = timeadd(timestamp(), "17520h")
+}
+resource "azurerm_key_vault_secret" "fact_cron_trigger_app_reg_id" {
+  name         = "fact-cron-trigger-app-reg-id"
+  value        = data.azuread_application.fact_cron_trigger_app_reg.client_id
+  key_vault_id = module.key_vault.key_vault_id
+  tags = merge(var.common_tags, {
+    "source" : "calculated from ${local.fact_cron_trigger_app_reg_name} app reg"
   })
   content_type    = "Calculated Secret"
   expiration_date = timeadd(timestamp(), "17520h")
