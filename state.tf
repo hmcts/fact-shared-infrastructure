2f1bbb5905625aae2a0bd6532c491ed9336858fa
josh-uk <joshua.blackmoor@justice.gov.uk>
2026-08-19 13:49:46 +0100

    FACT-2983: Remove explicit null provider constraint

--- state.tf
+++ state.tf
@@ -14,10 +14,6 @@ terraform {
       source  = "hashicorp/http"
       version = "3.6.1"
     }
-    null = {
-      source  = "hashicorp/null"
-      version = "3.3.1"
-    }
     random = {
       source  = "hashicorp/random"
       version = "3.9.0"
